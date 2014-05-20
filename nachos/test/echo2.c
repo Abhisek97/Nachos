622eed45afea8e049e6246c3c2ada7ab0d689ac4
danielknapp <dknapp@ucsd.edu>
2014-05-19 17:19:30 -0700

    Still testing stuff with echo2.c

--- nachos/test/echo2.c
+++ nachos/test/echo2.c
@@ -9,9 +9,13 @@ int main(int argc, char** argv)
   printf("argc address: %d\n", &argc);
   printf("argv address: %d\n", &argv);
   printf("local var i address: %d\n", &i);
+
+  char * fmt = "arg %d: %s\n";
+  printf("address of fmt: %d\n", &fmt);
+  printf("address stored in fmt: %d\n", fmt);
   
   for (i=0; i<argc; i++)
-    printf("arg %d: %s\n", i, argv[i]);
+    printf(fmt, i, argv[i]);
 
   return 0;
 }
