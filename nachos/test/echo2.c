81779afa0a04406866ab4be67e1265986f4e805c
danielknapp <dknapp@ucsd.edu>
2014-05-19 18:06:59 -0700

    Made some more modifications to echo2.c

--- nachos/test/echo2.c
+++ nachos/test/echo2.c
@@ -13,6 +13,14 @@ int main(int argc, char** argv)
   char * fmt = "arg %d: %s\n";
   printf("address of fmt: %d\n", &fmt);
   printf("address stored in fmt: %d\n", fmt);
+  if (argc > 0) {
+    printf("address stored in argv[0]: %dn", argv);
+    printf("string in argv[0]: %sn", argv[0]);
+  }
+  if (argc > 1) {
+    printf("address stored in argv[1]: %dn", argv+1);
+    printf("string in argv[1]: %sn", argv[1]);
+  }
   
   for (i=0; i<argc; i++)
     printf(fmt, i, argv[i]);
