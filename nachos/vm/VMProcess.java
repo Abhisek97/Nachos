b13a5fe09bafbad763cbb893f0f569797718e07f
gabriellaerwin <gerwin@ucsd.edu>
2014-06-13 17:14:25 -0700

    Implemented TLB Management. It uses handlePageFault which is not implemented.. yet.

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -4,6 +4,7 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.Random;
 
 /**
  * A <tt>UserProcess</tt> that supports demand-paging.
@@ -22,6 +23,15 @@ public class VMProcess extends UserProcess {
 	 */
 	public void saveState() {
 		super.saveState();
+		
+		for (int i = 0; i < Machine.processor().getTLBSize(); i++) {
+	        TranslationEntry entry = Machine.processor().readTLBEntry(i);
+	        if (entry.valid) {
+	        	entry.valid = false;
+	            Machine.processor().writeTLBEntry(i, entry);
+	            pageTable[entry.vpn] = new TranslationEntry(entry);
+	        }
+	    }
 	}
 
 	/**
@@ -30,6 +40,16 @@ public class VMProcess extends UserProcess {
 	 */
 	public void restoreState() {
 		super.restoreState();
+		
+		//Is this needed??
+		for (int i = 0; i < Machine.processor().getTLBSize(); i++) {
+	        TranslationEntry entry = Machine.processor().readTLBEntry(i);
+	        if (entry.valid) {
+	        	entry.valid = false;
+	            Machine.processor().writeTLBEntry(i, entry);
+	            pageTable[entry.vpn] = new TranslationEntry(entry);
+	        }
+	    }
 	}
 
 	/**
@@ -60,12 +80,85 @@ public class VMProcess extends UserProcess {
 		Processor processor = Machine.processor();
 
 		switch (cause) {
+		
+		case Processor.exceptionTLBMiss:
+			int e = Machine.processor().readRegister(Processor.regBadVAddr);
+            handleTLBMiss(e);
+            break;
+		case Processor.exceptionPageFault:
+            break;
 		default:
 			super.handleException(cause);
 			break;
 		}
 	}
 
+	
+	protected void handleTLBMiss(int miss){
+        
+        int tlbToBeSwapped = -1;
+        
+        //loops through all the entries of the tlb checking to see if its valid.
+        //If not valid, designate it as the one to be swapped out
+        for(int i = 0; i < Machine.processor().getTLBSize(); i++){
+        	
+        	//If we haven't found a tlb to be swapped
+            if(tlbToBeSwapped == -1){
+                
+                TranslationEntry entry = Machine.processor().readTLBEntry(i);
+                
+                //Set the tlb to be swapped as this one cuz it's not valid
+                if (!entry.valid)
+                    tlbToBeSwapped = i;
+            }
+        }
+        
+        //If all tlb entries are valid, pick a random one to swap out
+        if(tlbToBeSwapped == -1){
+        	
+        	//Need to seed?
+        	Random random = new Random();
+        	tlbToBeSwapped = random.nextInt(Machine.processor().getTLBSize());
+        	
+        	TranslationEntry swapEntry = Machine.processor().readTLBEntry(tlbToBeSwapped);
+        	
+        	//Check entry against page table entry for validity
+        	if (swapEntry.valid) {
+                this.pageTable[swapEntry.vpn].dirty = swapEntry.dirty;
+                this.pageTable[swapEntry.vpn].used = swapEntry.used;
+            }
+        }
+        
+        //Get the translation entry for the missed entry
+        TranslationEntry entryToBeAdded = new TranslationEntry(pageTable[miss]);
+        
+        while (!entryToBeAdded.valid) {
+            handlePageFault(miss);
+            entryToBeAdded = new TranslationEntry(pageTable[miss]);
+        }
+        
+        //Finally add the entry in the TLB :) 
+        Machine.processor().writeTLBEntry(tlbToBeSwapped, entryToBeAdded);
+	}
+	
+	protected void handlePageFault(int fault){
+		
+		TranslationEntry faultEntry = pageTable[fault];
+		
+		faultEntry.vpn = fault;
+		faultEntry.valid = true;
+		//fault ppn?
+		
+		//VMKernel.invertedPageTable[ppn] = this;
+		
+		for(int i=0; i<coff.getNumSections(); i++) {
+			
+			CoffSection sect = coff.getSection(i);
+			
+			
+		}
+	}
+	
 	private static final int pageSize = Processor.pageSize;
 
 	private static final char dbgProcess = 'a';
