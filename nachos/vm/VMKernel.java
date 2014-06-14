d0e1a5e24c4d384c8cb2bf24cbeb13388f949fe0
gabriellaerwin <gerwin@ucsd.edu>
2014-06-13 21:54:18 -0700

    Implemention of swapOut and swapIn (in progress)

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -1,6 +1,8 @@
 package nachos.vm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import nachos.machine.*;
 import nachos.threads.*;
@@ -17,6 +19,11 @@ public class VMKernel extends UserKernel {
 	public VMKernel() {
 		super();
 
+		swapSpace = new HashMap<MetaData, TranslationEntry>();
+		pagesCanBeSwapped = new ArrayList<TranslationEntry>();
+		freePages = new LinkedList<Integer>();
+		pinnedPages = new ArrayList<Integer>();
+		
 	}
 
 	/**
@@ -32,6 +39,8 @@ public class VMKernel extends UserKernel {
 		{
 			iPageTable[i] = null;
 		}
+		
+		swapFile = ThreadedKernel.fileSystem.open(swapFile.getName(), true);
 		
 		super.initialize(args);
 	}
@@ -72,6 +81,10 @@ public class VMKernel extends UserKernel {
 	 * Terminate this kernel. Never returns.
 	 */
 	public void terminate() {
+		//Close swapfile
+		swapFile.close();
+        ThreadedKernel.fileSystem.remove(swapFile.getName());
+		
 		super.terminate();
 	}
 	
@@ -89,14 +102,95 @@ public class VMKernel extends UserKernel {
         pinLock.release();
     }
 	
-	public boolean contains(Integer i){
+	public boolean contains(TranslationEntry i){
 		pinLock.acquire();
 		boolean result = pinnedPages.contains(i);
 		pinLock.release();
 		return result;
 	}
 	
+	
+	public int swapOut(){
+        TranslationEntry swapPage = null;
+
+        if(!pagesCanBeSwapped.isEmpty()){
+            int i =0;
+            while(i< pagesCanBeSwapped.size())
+            {
+                //swapLock.acquire();
+                swapPage = pagesCanBeSwapped.get(i);
+                //swapLock.release();
+                
+                if(!contains(swapPage)){
+                    //swapLock.acquire();
+                    swapPage = pagesCanBeSwapped.remove(i);
+                    //swapLock.release();
+                    break;
+                }
+                i = (i+1) % (pagesCanBeSwapped.size());
+            }
+        }
+        int removed = -1;
+        
+        iptLock.acquire();
+        for(int i = 0; i < iPageTable.length; i++){
+        	
+        	//index is the ppn
+            if(i == swapPage.ppn){
+                removed = i;
+                break;
+            }
+        }
+
+        if(removed!=-1)
+        	iPageTable[removed] = null;
+        iptLock.release();
+
+        int size = Processor.pageSize;
+        
+        byte[] pageContents = new byte[size];
+        byte[] memory = Machine.processor().getMemory();
+
+        System.arraycopy(memory, swapPage.ppn*size, pageContents, 0, size);
+        int loc = freePages.removeFirst()*size;
+        swapFile.write(loc, pageContents, 0, size);
+        MetaData removedPage = iPageTable[removed];
+        swapSpace.put(removedPage,swapPage);
+        //
+        return swapPage.ppn;
+    }
+	
+	public int swapIn(int vpn, VMProcess process){
+        TranslationEntry freeEntry = getFreePage(vpn);
+        MetaData data = new MetaData(vpn, process, false);
+        
+        int loc = diskLoc.get(data);
+        int size = Processor.pageSize;
+        
+        byte[] pageContent = new byte[size];
+
+
+        swapFile.read(loc, pageContent,0,size);
+        byte[] mem = Machine.processor().getMemory();
+        System.arraycopy(pageContent,0,mem,freeEntry.ppn*size,size);
+        swapSpace.remove(data);
+        freePages.add(freeEntry.ppn);
+        
+        diskLoc.remove(data);
+
+        return freeEntry.ppn;
+
+    }
+	
+	//Define Variables
+	//*******************************************************************************************
+	
 	protected ArrayList<Integer> pinnedPages;
+	protected ArrayList<TranslationEntry> pagesCanBeSwapped;
+	protected static LinkedList<Integer> freePages;
+	protected HashMap<MetaData, TranslationEntry> swapSpace;
+	protected HashMap<MetaData, Integer> diskLoc; 
+	public static OpenFile swapFile;
 	private Lock pinLock;
 	
 	private Lock iptLock;
