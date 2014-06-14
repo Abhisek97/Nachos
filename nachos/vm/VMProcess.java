220f4e210d6387349df2ecf6ea8bf844d84c4b7a
danielknapp <dknapp@ucsd.edu>
2014-06-13 21:57:07 -0700

    Partially implemented handlePageFault

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -4,6 +4,8 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 /**
@@ -15,6 +17,7 @@ public class VMProcess extends UserProcess {
 	 */
 	public VMProcess() {
 		super();
+		spnTable = new Integer[pageTable.length];
 	}
 
 	/**
@@ -39,7 +42,7 @@ public class VMProcess extends UserProcess {
 	 * <tt>UThread.restoreState()</tt>.
 	 */
 	public void restoreState() {
-		super.restoreState();
+//		super.restoreState();
 		
 		//Is this needed??
 		for (int i = 0; i < Machine.processor().getTLBSize(); i++) {
@@ -59,7 +62,81 @@ public class VMProcess extends UserProcess {
 	 * @return <tt>true</tt> if successful.
 	 */
 	protected boolean loadSections() {
-		return super.loadSections();
+//		return super.loadSections();
+		if (numPages > Machine.processor().getNumPhysPages()) {
+			coff.close();
+			Lib.debug(dbgProcess, "\tinsufficient physical memory");
+			return false;
+		}
+		
+		// load sections
+		for (int s = 0; s < coff.getNumSections(); s++) {
+			CoffSection section = coff.getSection(s);
+
+			Lib.debug(dbgProcess, "\tinitializing " + section.getName()
+					+ " section (" + section.getLength() + " pages)");
+
+			for (int i = 0; i < section.getLength(); i++) {
+				int vpn = section.getFirstVPN() + i;
+
+				// for now, just assume virtual addresses=physical addresses
+				//section.loadPage(i, vpn);
+				
+				//Modification for Proj 2
+				TranslationEntry entry = pageTable[vpn];
+				
+				UserKernel.physPageMutex.P();
+				Integer thePage = null;
+				try
+			    {
+			    	thePage = UserKernel.physicalPages.removeFirst();
+
+			    }
+			    catch (NoSuchElementException e)
+			    {
+			    	// Not enough physical memory left, so give the pages back
+			    	// to the processor and return false
+			    	unloadSections();
+			    	return false;
+			    }
+				UserKernel.physPageMutex.V();
+				
+				entry.ppn = thePage;
+				entry.valid = true;
+				entry.readOnly = section.isReadOnly();
+				section.loadPage(i, entry.ppn);
+			}
+		}
+		
+		// Modification for Proj 2: Allocating pages for text section, stack, arguments
+		// i starts at numPages-9 for only modifying the pageTable entries for the 8
+		// pages of stack and 1 additional page for arguments
+		// TODO: May need to allocate Stack pages at the end of the pageTable
+		//       from high to low memory.
+		for (int i = numPages-9; i < numPages; i++) {
+		    TranslationEntry entry = pageTable[i];
+		    UserKernel.physPageMutex.P();
+		    Integer pageNumber = null;
+		    try
+		    {
+		    	pageNumber = UserKernel.physicalPages.removeFirst();
+
+		    }
+		    catch (NoSuchElementException e)
+		    {
+		    	// Not enough physical memory left, so give the pages back
+		    	// to the processor and return false
+		    	unloadSections();
+		    	return false;
+		    }
+		    
+		    
+		    UserKernel.physPageMutex.V();
+		    entry.ppn = pageNumber;
+		    entry.valid = true;
+		}
+
+		return true;
 	}
 
 	/**
@@ -149,6 +226,52 @@ public class VMProcess extends UserProcess {
 		
 		faultEntry.vpn = fault;
 		faultEntry.valid = true;
+		
+		// kernel needs a static condition variable for all pages pinned
+		int ppn = -1;
+		int originalHand = clockhand;
+		
+		do
+		{
+			
+			try {
+				ppn = VMKernel.physicalPages.removeFirst().intValue();
+			} catch (Exception e) { }
+			
+			if (ppn < 0)
+			{
+				VMKernel.MetaData currMet = VMKernel.iPageTable[clockhand];
+				
+				if(!currMet.pinned)
+				{
+					// possible to be evicted if not pinned
+					TranslationEntry currEntry = currMet.ownProcess.pageTable[currMet.vpn];
+					if(currEntry.used)
+						currEntry.used = false;
+					else {
+						if (currEntry.dirty) {
+							// evict and swap
+							
+						}
+						else {
+							// evict without swapping
+							currEntry.valid = false;
+							// 
+							
+							//
+						}
+					}
+				}
+				
+				clockhand = (clockhand + 1) % VMKernel.iPageTable.length;
+				if (clockhand == originalHand)
+				{
+					// condition variable wait
+				}
+			}
+			
+		} while (ppn < 0);
+		
 		//fault ppn?
 		
 		//VMKernel.invertedPageTable[ppn] = this;
@@ -160,6 +283,10 @@ public class VMProcess extends UserProcess {
 			
 		}
 	}
+	
+	private static int clockhand = 0;
+	
+	private Integer[] spnTable;
 	
 	private static final int pageSize = Processor.pageSize;
 
